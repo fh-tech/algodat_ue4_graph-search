0a03de3753a3c7628640797725b79d7dca248787
Viktor <viktor.leher@gmail.com>
2018-05-21 19:44:35 +0200

    finished functionality for frontend and made basic styling so one can see the path at least

--- graphsearch-server/src/main/resources/public/dijkstra.js
+++ graphsearch-server/src/main/resources/public/dijkstra.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
+    // add autocomplete on keyup for the two inputs
    $("#station_to").on('keyup', function (e) {
        feedAwesomeplete(e.target);
    });
     $("#station_from").on('keyup', function (e) {
         feedAwesomeplete(e.target);
     });
+
+    // prevent page reload and handle call to api
+    $("#dijkstra_form").submit(async function(e) {
+        // prevent the reload
+        e.preventDefault();
+        let from = e.target.elements["station_from"].value;
+        let to = e.target.elements["station_to"].value;
+        let path = await find_shortest(from, to);
+        showPath(path);
+    })
 });
 
 async function feedAwesomeplete(target) {
@@ -17,7 +28,6 @@ async function feedAwesomeplete(target) {
     }
 
     for(let i = 0; i < 10 && i < stations.length; i++) {
-        console.log("got here");
         let option = document.createElement("option");
         option.setAttribute("value", stations[i]);
         dataList.appendChild(option);
@@ -28,4 +38,58 @@ async function fetchStations(station) {
     let response = await fetch(`api/graph/name?station=${station}`);
     let json = await response.json();
     return json.map(obj => {return obj.stationName});
+}
+
+async function find_shortest(from, to) {
+    let path = await fetch(`api/graph/path?from=${from}&to=${to}`);
+    return await path.json();
+
+}
+
+function showPath(path) {
+    let container = $("#result");
+    let lastLine = null;
+    let sumTime = 0;
+
+    for(let segment of path) {
+        addStationCol(segment.prev.stationName, container);
+
+        let line = segment.line.name;
+        addLineDurCol(segment.duration, line, lastLine, container);
+        sumTime += (lastLine != null && line !== lastLine) ? segment.duration + 5 : segment.duration;
+        lastLine = line;
+    }
+    // add last station at end
+    addStationCol(path[path.length - 1].next.stationName, container);
+    // add sum time
+    let col = $("<div>", {"class": "col-12 my-2"});
+    let time = $("<span>", {"class": "font-weight-bold"}).text("Total time: " + sumTime + "min");
+    col.append(time);
+    container.append(col);
+}
+
+function addStationCol(fromStation, container) {
+    let col = $("<div>", {"class": "col-12 station_segment my-2"});
+    let contain = $("<div>", {"class": "path_contain text-center"});
+    let from = $("<span>", {"class": "font-weight-bold"}).text(fromStation);
+    contain.append(from);
+    col.append(contain);
+    container.append(col);
+}
+
+function addLineDurCol(duration, line, lastLine, container) {
+    if(lastLine !== null && lastLine !== line) {
+        let col = $("<div>", {"class": "col-12 dur_segment my-2"});
+        let penalty =  $("<span>", {"class": "switch_penalty"}).text(lastLine + "-->" + line + " (5min)");
+        let contain = $("<div>", {"class": "path_contain text-center"});
+        contain.append(penalty);
+        col.append(contain);
+        container.append(col);
+    }
+    let col = $("<div>", {"class": "col-12 dur_segment my-2"});
+    let contain = $("<div>", {"class": "path_contain text-center"});
+    let line_dur = $("<span>").text("Line: " + line + "(" + duration +"min)");
+    contain.append(line_dur);
+    col.append(contain);
+    container.append(col);
 }
